59ca928d2c493f9f9c160d032b4fd9e5098a3215
frei480 <frei741@gmail.com>
2025-11-07 23:43:22 +0500

    create, delete, update, get link

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,10 +1,74 @@
+from contextlib import asynccontextmanager
+from datetime import datetime, timezone
+from uuid import uuid4
+
 import uvicorn
-from fastapi import FastAPI
+from database import get_session, init_db
+from fastapi import Depends, FastAPI, HTTPException
+from fastapi.responses import RedirectResponse
+from model import Link
+from sqlmodel import select
+from sqlmodel.ext.asyncio.session import AsyncSession
 
-app = FastAPI(title="Url shortener")
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await init_db()
+    yield
 
 
-@app.get("/sh")
+app = FastAPI(title="Url shortener", lifespan=lifespan)
+
+
+@app.post("/shorten", response_model=Link, status_code=201)
+async def create_short_url(
+    original_url: str, session: AsyncSession = Depends(get_session)
+):
+    exists_link = await session.exec(
+        select(Link).where(Link.original_url == original_url)
+    )
+    exists_link_obj = exists_link.first()
+
+    if exists_link_obj:
+        return exists_link_obj
+
+    short_link: str = uuid4().hex[8:]
+    while True:
+        check_short_link = await session.exec(
+            select(Link).where(Link.short_url == short_link)
+        )
+        if check_short_link.first() is None:
+            break
+        short_link = uuid4().hex[8:]
+    link = Link(original_url=original_url, short_url=short_link)
+    session.add(link)
+    await session.commit()
+    await session.refresh(link)
+    return link
+
+
+@app.get("/{short_link}", response_class=RedirectResponse)
+async def redirect_to_original_url(
+    short_link: str, session: AsyncSession = Depends(get_session)
+):
+    link = await session.exec(select(Link).where(Link.short_url == short_link))
+    link_obj = link.first()
+    if not link_obj:
+        raise HTTPException(status_code=404, detail="Link not found")
+
+    if link_obj.expires_at < datetime.now(timezone.utc):
+        await session.delete(link)
+        await session.commit()
+        raise HTTPException(status_code=410, detail="Link has expired")
+
+    link_obj.update_access_time()
+
+    session.add(link)
+    await session.commit()
+
+    return RedirectResponse(link_obj.original_url, status_code=302)
+
+
 @app.get("/health", status_code=200)
 def health_check():
     return {"status": "ok"}
