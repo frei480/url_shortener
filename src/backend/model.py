f1457d7b85faa7d70b97ec14ead0971bd412be6d
frei480 <frei741@gmail.com>
2025-11-08 14:51:43 +0500

    fix timezone

--- src/backend/model.py
+++ src/backend/model.py
@@ -8,14 +8,19 @@ class Link(SQLModel, table=True):
     id: UUID = Field(default_factory=uuid4, primary_key=True, index=True)
     original_url: str = Field(index=True)
     short_url: str = Field(unique=True, index=True)
-    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    created_at: datetime = Field(
+        default_factory=lambda: datetime.now(timezone.utc).replace(tzinfo=None)
+    )
     last_accessed_at: datetime = Field(
-        default_factory=lambda: datetime.now(timezone.utc)
+        default_factory=lambda: datetime.now(timezone.utc).replace(tzinfo=None)
     )
     expires_at: datetime = Field(
-        default_factory=lambda: datetime.now(timezone.utc) + timedelta(days=365)
+        default_factory=lambda: datetime.now(timezone.utc).replace(tzinfo=None)
+        + timedelta(days=365)
     )
 
     def update_access_time(self):
-        self.last_accessed_at = datetime.now(timezone.utc)
-        self.expires_at = datetime.now(timezone.utc) + timedelta(days=365)
+        self.last_accessed_at = datetime.now(timezone.utc).replace(tzinfo=None)
+        self.expires_at = datetime.now(timezone.utc).replace(tzinfo=None) + timedelta(
+            days=365
+        )
